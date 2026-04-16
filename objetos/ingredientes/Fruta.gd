dcc70af01fceb75f4cc1a83e9275ee8492bffa7f
Islan <islan12victhor@gmail.com>
2026-04-16 17:50:49 -0300

    Sprite Frutas

--- objetos/ingredientes/Fruta.gd
+++ objetos/ingredientes/Fruta.gd
@@ -1,17 +1,25 @@
 extends Ingrediente
 class_name Fruta
 
+enum sprites_frutas {
+	GREENET,
+	ORANGINE,
+	PURPLEZITE,
+	REDJAN,
+	REDJAN2
+}
+
 @onready var sprite : Sprite2D = $Sprite2D
 @onready var colisao : CollisionShape2D = $CollisionShape2D
 
-var tipos_frutas : Array = ["Orangine", "Purplezite", "Greenet", "Redarang_2"]
+var tipos_frutas : Array = ["Orangine", "Purplezite", "Greenet", "Redjan2"]
 
 func setup(tipo_fruta:String = "random"):
 	if tipo_fruta == "random":
 		nome = tipos_frutas.pick_random()
 	else:
 		nome = tipo_fruta
-	sprite.texture = load("res://objetos/ingredientes/%s.png" % [nome.to_lower()])
+	sprite.frame = sprites_frutas[nome.to_upper()]
 
 func levantar():
 	colisao.disabled = true
