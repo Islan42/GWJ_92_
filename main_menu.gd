cef13d8ac2d402dcf0595f06503aaee6baf1de0d
Islan <islan12victhor@gmail.com>
2026-04-25 07:35:31 -0300

    Main Menu 1.1

--- main_menu.gd
+++ main_menu.gd
@@ -2,5 +2,8 @@ extends Control
 
 const MAIN = preload("res://main.tscn")
 
+func _ready():
+	$AnimationPlayer.play("idle")
+
 func _on_new_game_button_pressed():
 	get_tree().change_scene_to_packed(MAIN)
